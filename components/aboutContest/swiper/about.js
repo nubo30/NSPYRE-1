e0ab01b4397ebe13586892c31b19d9d4544880c3
Yank Carlos <yank_carlos_30@hotmail.com>
2019-07-29 15:51:14 -0400

    eliminacion del icono en <About the Contest>>

--- components/aboutContest/swiper/about.js
+++ components/aboutContest/swiper/about.js
@@ -13,6 +13,8 @@ import {
 } from 'native-base';
 import _ from 'lodash'
 
+import { Entypo } from '@expo/vector-icons'
+
 // I sinned contest introduction
 export default function About(props) {
     const { contest, _setModalVisibleAboutTheContest } = props
@@ -29,22 +31,12 @@ export default function About(props) {
                 <CardItem onPress={() => { _setModalVisibleAboutTheContest(true) }} header bordered style={{ borderTopRightRadius: 15, borderTopLeftRadius: 15, justifyContent: 'center', alignItems: 'center' }}>
                     <H1 onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ color: "#D82B60" }}>About the Contest</H1>
                 </CardItem>
-                <CardItem onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ borderBottomLeftRadius: 15, borderBottomEndRadius: 15 }}>
-                    <List onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ width: "100%" }}>
-                        <ListItem avatar>
-                            <Left>
-                                <Thumbnail onPress={() => { _setModalVisibleAboutTheContest(true) }}
-                                    style={{ bottom: 5 }} source={{ uri: "https://i-h1.pinimg.com/564x/e3/2d/b2/e32db2260c53dddbce0219f94b0a9880.jpg?b=t" }} />
-                            </Left>
-                            <Body style={{ borderBottomColor: "#fff" }}>
-                                <Text onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ color: "#BDBDBD" }}>
-                                    {_.truncate(contest.general.description, { length: 40, separate: '...' })}
-                                </Text>
-                                <Text onPress={() => { _setModalVisibleAboutTheContest(true) }}
-                                    style={{ color: "#D82B60", textDecorationLine: "underline", top: 3 }}>Know more</Text>
-                            </Body>
-                        </ListItem>
-                    </List>
+                <CardItem onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ borderBottomLeftRadius: 15, borderBottomEndRadius: 15, flexDirection: 'column', padding : 5 }}>
+                    <Text onPress={() => { _setModalVisibleAboutTheContest(true) }} style={{ color: "#BDBDBD" }}>
+                        {_.truncate(contest.general.description, { length: 40, separate: '...' })}
+                    </Text>
+                    <Text onPress={() => { _setModalVisibleAboutTheContest(true) }}
+                        style={{ color: "#D82B60", textDecorationLine: "underline", top: 3 }}>Know more</Text>
                 </CardItem>
             </Card>
         </TouchableHighlight>
